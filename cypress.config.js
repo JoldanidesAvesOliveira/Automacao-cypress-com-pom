42f5f513fd5683dc49a2ca7da3b48a9b2915705e
Joldanides <guil.sub3@gmail.com>
2026-06-02 19:36:20 -0300

    Configurando baseUrl

--- cypress.config.js
+++ cypress.config.js
@@ -7,5 +7,9 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+     // Configurações de URL e padrões de teste
+    baseUrl: "https://www.saucedemo.com/",
+    specPattern: "cypress/e2e/**/*.cy.js",
+    supportFile: "cypress/support/e2e.js",
   },
 });
