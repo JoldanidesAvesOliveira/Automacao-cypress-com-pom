ef84f8fa9d110513649df6ebd7e6ca0ccb95b3ec
Joldanides <guil.sub3@gmail.com>
2026-06-02 14:07:48 -0300

    Criando classe loginActions com seus metodos de ações

---
+++ cypress/support/actioins/login/loginActions.js
@@ -0,0 +1,16 @@
+import { loginPageLocators as lPL } from '../locators/login_page';
+class LoginActions {
+visitPage(){
+    cy.visit("/");
+}
+fillLoginForm(username, password){
+    cy.get(lPL.Username).type(username);
+    cy.get(lPL.Password).type(password);
+        
+}
+clickLoginButton(){
+    cy.get(lPL.LoginButton).click();
+}
+}
+
+export default new LoginActions();
