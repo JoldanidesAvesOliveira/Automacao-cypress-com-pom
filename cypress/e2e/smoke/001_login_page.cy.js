98d889d718602ab32a213550e8abc1724c1762bb
Joldanides <guil.sub3@gmail.com>
2026-06-03 08:03:39 -0300

    refactor

--- cypress/e2e/smoke/001_login_page.cy.js
+++ cypress/e2e/smoke/001_login_page.cy.js
@@ -1,13 +1,13 @@
-import loginActions from '../../support/actioins/login/loginActions';   
+import LoginActions from '../../support/actions/login/loginActions';
 
 describe('TC001 Login Page',() => {
     beforeEach(() => {
-        loginActions.visitPage();
+        LoginActions.visitPage();
     });
     describe('TC001.01 Login with valid credentials', () => {
         it('should login successfully', () => {
-            loginActions.fillLoginForm("standard_user", "secret_sauce");
-            loginActions.clickLoginButton();
+            LoginActions.fillLoginForm("standard_user", "secret_sauce");
+            LoginActions.clickLoginButton();
             cy.url().should('include', '/inventory.html');
         });
     });
