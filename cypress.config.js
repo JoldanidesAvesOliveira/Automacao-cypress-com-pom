caf24165a106bbe8d3b76062c51ba734a0fb9147
Joldanides <guil.sub3@gmail.com>
2026-06-09 19:43:09 -0300

    Configurando cypress.config.js e package.json

--- cypress.config.js
+++ cypress.config.js
@@ -5,11 +5,19 @@ module.exports = defineConfig({
 
   e2e: {
     setupNodeEvents(on, config) {
+      return config;
       // implement node event listeners here
     },
      // Configurações de URL e padrões de teste
     baseUrl: "https://www.saucedemo.com/",
     specPattern: "cypress/e2e/**/*.cy.js",
     supportFile: "cypress/support/e2e.js",
+
+    video: true,
+    screenshotOnRunFailure: true,
+    retries:{
+      runMode: 2,
+      openMode: 0
+    }
   },
 });
