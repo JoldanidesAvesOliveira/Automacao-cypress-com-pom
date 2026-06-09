8b8e29a9a9b64af3d90aaf2b2c0c9fca9d8d97be
Joldanides <guil.sub3@gmail.com>
2026-06-09 19:59:32 -0300

    Refactor branch anterior

--- cypress.config.js
+++ cypress.config.js
@@ -4,12 +4,26 @@ module.exports = defineConfig({
   allowCypressEnv: false,
 
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-     // Configurações de URL e padrões de teste
+
+ // Configurações de URL e padrões de teste
     baseUrl: "https://www.saucedemo.com/",
     specPattern: "cypress/e2e/**/*.cy.js",
     supportFile: "cypress/support/e2e.js",
+
+
+    setupNodeEvents(on, config) {
+      return config;
+      // implement node event listeners here
+    },
+
+    video: true,
+
+    screenshotOnRunFailure: true,
+
+    retries: {
+      runMode: 2,
+      openMode: 0
+    }
+    
   },
 });
