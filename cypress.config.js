11817559a133f8302596fbb08b29739538b6643b
Joldanides <guil.sub3@gmail.com>
2026-06-10 19:12:58 -0300

    refactor: cypress.yml, package.json e cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,28 +2,39 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   allowCypressEnv: false,
+  reporter: "cypress-multi-reporters",
+  reporterOptions: {
+    reporterEnabled: "mochawesome, mocha-junit-reporter",
+    mochawesomeReporterOptions: {
+      reportDir: "cypress/reports/mochawesome",
+      overwrite: false,
+      html: false,
+      json: true
+    },
+    mochaJunitReporterReporterOptions: {
+      mochaFile: "cypress/reports/junit/results-[hash].xml"
+    }
+  },
+  video: true,
+  videoUploadOnPasses: false,
+  screenshotsFolder: "cypress/screenshots",
+  videosFolder: "cypress/videos",
 
   e2e: {
-
- // Configurações de URL e padrões de teste
+    // Configurações de URL e padrões de teste
     baseUrl: "https://www.saucedemo.com/",
     specPattern: "cypress/e2e/**/*.cy.js",
     supportFile: "cypress/support/e2e.js",
-
 
     setupNodeEvents(on, config) {
       return config;
       // implement node event listeners here
     },
 
-    video: true,
-
     screenshotOnRunFailure: true,
-
     retries: {
       runMode: 2,
       openMode: 0
     }
-    
-  },
+  }
 });
