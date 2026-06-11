0ee871a388e5e9406c0b6ed1b3a58483e861f9c3
Joldanides <guil.sub3@gmail.com>
2026-06-11 13:19:58 -0300

    Refactor: import loginActions

--- cypress/e2e/smoke/001_login_page.cy.js
+++ cypress/e2e/smoke/001_login_page.cy.js
@@ -1,21 +1,21 @@
-import loginActions from "../../support/actions/login/loginActions";
+import LoginActions from "../../support/actions/login/loginActions";
 
 describe('TC001 Login Page',() => {
     beforeEach(() => {
-        loginActions.visitPage();
+        LoginActions.visitPage();
     });
     describe('TC001.01 Login with valid credentials', () => {
         it('should login successfully', () => {
-            loginActions.fillLoginForm("standard_user", "secret_sauce");
-            loginActions.clickLoginButton();
+            LoginActions.fillLoginForm("standard_user", "secret_sauce");
+            LoginActions.clickLoginButton();
             cy.url().should('include', '/inventory.html');
         });
     });
      describe('TC001.02 Login with invalid credentials', () => {
         it('should display an error message with invalid credentials', () => {
             // Fill in the login form with invalid credentials
-            loginActions.fillLoginForm("invalid_user", "invalid_password");
-            loginActions.clickLoginButton();
+            LoginActions.fillLoginForm("invalid_user", "invalid_password");
+            LoginActions.clickLoginButton();
             cy.get("[data-test='error']").should("be.visible").and("contain", "Epic sadface: Username and password do not match any user in this service");
         });
     });
