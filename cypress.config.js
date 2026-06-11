661bfb31f7fa1419c6d260e199510bdb8b2b07a7
Joldanides <guil.sub3@gmail.com>
2026-06-11 10:23:33 -0300

    Refactor: Usando AI para refatorar CI

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,6 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
   reporter: "cypress-multi-reporters",
   reporterOptions: {
     reporterEnabled: "mochawesome, mocha-junit-reporter",
@@ -12,29 +11,28 @@ module.exports = defineConfig({
       json: true
     },
     mochaJunitReporterReporterOptions: {
-      mochaFile: "cypress/reports/junit/results-[hash].xml"
+      mochaFile: "cypress/reports/junit/results-[hash].xml",
+      toConsole: false
     }
   },
   video: true,
   videoUploadOnPasses: false,
+  trashAssetsBeforeRuns: true,
   screenshotsFolder: "cypress/screenshots",
   videosFolder: "cypress/videos",
 
   e2e: {
-    // Configurações de URL e padrões de teste
-    baseUrl: "https://www.saucedemo.com/",
+    baseUrl: "https://www.saucedemo.com",
     specPattern: "cypress/e2e/**/*.cy.js",
     supportFile: "cypress/support/e2e.js",
-
     setupNodeEvents(on, config) {
       return config;
-      // implement node event listeners here
     },
-
     screenshotOnRunFailure: true,
     retries: {
       runMode: 2,
       openMode: 0
-    }
+    },
+    watchForFileChanges: false
   }
 });
