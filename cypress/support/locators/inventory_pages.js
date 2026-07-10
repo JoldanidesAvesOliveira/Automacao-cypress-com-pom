4ed2c07951db9a477ec31fb8e547bb5c3bb4b54e
Joldanides <gil.sub3@gmail.com>
2026-07-10 15:45:27 -0300

    feat: adicionando identificadores da página inventory

---
+++ cypress/support/locators/inventory_pages.js
@@ -0,0 +1,22 @@
+export const inventoryPageLocators = {
+
+ backpack:"[data-test='item-4-title-link']",
+ backpaackAddToCart:"[data-test='add-to-cart-sauce-labs-backpack']",
+ boltTshirt:"[data-test='item-1-title-link']",
+ boolTshirtAddToCart:"[data-test='add-to-cart-sauce-labs-bolt-t-shirt']",   
+ onesie:"[data-test='item-2-title-link']",
+ onesieAddToCart:"[data-test='add-to-cart-sauce-labs-onesie']",
+ bikeLight:"[data-test='item-0-title-link']",
+ bikeLightAddToCart:"[data-test='add-to-cart-sauce-labs-bike-light']",
+ fleeceJacket:"[data-test='item-5-title-link']",
+ fleeceJacketAddToCart:"[data-test='add-to-cart-sauce-labs-fleece-jacket']",
+ allTheThingsTshirt:"[data-test='item-3-title-link']",
+ allTheThingsTshirtAddToCart:"[data-test='add-to-cart-sauce-labs-allthethings-t-shirt']",
+
+cartIcon:"[class='shopping_cart_link']",
+nameAtoZ:"[data-test='product_sort_container'] option[value='az']",
+nameZtoA:"[data-test='product_sort_container'] option[value='za']",
+priceLowToHigh:"[data-test='product_sort_container'] option[value='lohi']",
+priceHighToLow:"[data-test='product_sort_container'] option[value='hilo']",
+burgerMenu:"[data-test='header-container']"
+};
